2ffe1f36e3a48a64d892e6f923cf73de8625532c
Ishita-J <ishita.jinturkar@gmail.com>
2024-04-15 15:41:20 +0530

    the web app is ready

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
 
 app = Flask(__name__)
 regmodel = pickle.load(open("regmodel.pkl", "rb"))
@@ -15,13 +14,27 @@ def home():
 
 @app.route("/predict_api", methods=["POST"])
 def predict_api():
-    data = request.json["data"]
-    print(data)
-    print(np.array(list(data.values())).reshape(1, -1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
-    output = regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    try:
+        data = request.json["data"]
+        new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+        output = regmodel.predict(new_data)
+        return jsonify({"prediction": output[0]})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = [float(x) for x in request.form.values()]
+        final_input = scalar.transform(np.array(data).reshape(1, -1))
+        output = regmodel.predict(final_input)[0]
+        return render_template(
+            "home.html",
+            prediction_text="The House price prediction is {}".format(output),
+        )
+    except Exception as e:
+        return render_template("home.html", prediction_text=f"Error: {str(e)}")
 
 
 if __name__ == "__main__":
